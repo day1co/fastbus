acff24f28882ec7e9496c27ab19270a213b1dd36
Dongsu Jang <77693+iolo@users.noreply.github.com>
2022-02-13 23:22:10 +0900

    quick and dirty fix for new tsconfig

--- src/fast-bus.ts
+++ src/fast-bus.ts
@@ -6,7 +6,7 @@ const debug = Debug('fastbus');
 
 interface FastBusOpts {
   prefix?: string;
-  redis: ClientOpts;
+  redis?: ClientOpts;
   createRedisClient?: (ClientOpts) => RedisClient;
 }
 
@@ -20,8 +20,52 @@ export class FastBus {
   pubClient: RedisClient;
   subClient: RedisClient;
 
-  constructor(opts: FastBusOpts) {
-    this.init(opts);
+  constructor(opts?: FastBusOpts) {
+    const createRedisClient = opts?.createRedisClient ?? createClient;
+    this.pubClient = createRedisClient(opts?.redis);
+    this.subClient = createRedisClient(opts?.redis);
+    debug(`connect redis: ${opts?.redis?.host}:${opts?.redis?.port}/${opts?.redis?.db}`);
+
+    this.prefix = `${opts?.prefix ?? 'bus'}:${opts?.redis?.db ?? '0'}:`;
+
+    this.subscriptions = new EventEmitter();
+    this.subscriptions.setMaxListeners(Infinity);
+
+    this.subClient.on('pmessage', (pattern, channel, message) => {
+      debug('on pmessage', pattern, channel, message);
+      if (this.subscriptions.listenerCount(channel) === 0) {
+        debug('**ignore** no subscriber!', channel, message);
+        return;
+      }
+      if (channel !== message) {
+        debug('forward to all subscribers!', channel, message);
+        this.subscriptions.emit(channel, message);
+        return;
+      }
+      // TODO: scheduling?
+      const listener = this.subscriptions.listeners(channel)[0];
+      this.pubClient.rpop(channel, (err, message) => {
+        if (err) {
+          debug('**warning** rpop error!', err, channel);
+        }
+        if (message) {
+          listener(message);
+          debug('forward to the first subscriber!', channel);
+        } else {
+          debug('**warning** nothing to forward!', channel);
+        }
+      });
+    });
+
+    this.subClient.psubscribe(`${this.prefix}*`);
+    debug(`psubscribe ${this.prefix}*`);
+  }
+
+  destroy() {
+    debug('destroy');
+    this.unsubscribeAll();
+    this.subClient.end(true);
+    this.pubClient.end(true);
   }
 
   // 주의: redis pub/sub 은 db(key space)를 구분하지 않음 기본 db가 아니면 토픽 이름에 db 를 포함
@@ -65,55 +109,7 @@ export class FastBus {
     }
   }
 
-  init(opts: FastBusOpts) {
-    const createRedisClient = opts.createRedisClient || createClient;
-    this.pubClient = createRedisClient(opts.redis);
-    this.subClient = createRedisClient(opts.redis);
-    debug(`connect redis: ${opts.redis.host}:${opts.redis.port}/${opts.redis.db}`);
-
-    this.prefix = `${opts.prefix || 'bus'}:${opts.redis.db || '0'}:`;
-
-    this.subscriptions = new EventEmitter();
-    this.subscriptions.setMaxListeners(Infinity);
-
-    this.subClient.on('pmessage', (pattern, channel, message) => {
-      debug('on pmessage', pattern, channel, message);
-      if (this.subscriptions.listenerCount(channel) === 0) {
-        debug('**ignore** no subscriber!', channel, message);
-        return;
-      }
-      if (channel !== message) {
-        debug('forward to all subscribers!', channel, message);
-        this.subscriptions.emit(channel, message);
-        return;
-      }
-      // TODO: scheduling?
-      const listener = this.subscriptions.listeners(channel)[0];
-      this.pubClient.rpop(channel, (err, message) => {
-        if (err) {
-          debug('**warning** rpop error!', err, channel);
-        }
-        if (message) {
-          listener(message);
-          debug('forward to the first subscriber!', channel);
-        } else {
-          debug('**warning** nothing to forward!', channel);
-        }
-      });
-    });
-
-    this.subClient.psubscribe(`${this.prefix}*`);
-    debug(`psubscribe ${this.prefix}*`);
-  }
-
-  destroy() {
-    debug('destroy');
-    this.unsubscribeAll();
-    this.subClient.end(true);
-    this.pubClient.end(true);
-  }
-
-  static create(opts: FastBusOpts): FastBus {
+  static create(opts?: FastBusOpts): FastBus {
     return new FastBus(opts);
   }
 }
