f1579d4b7bac95fc62564df304eee75dcc36e21e
kooks7 <minjaekim@fastcampus.co.kr>
2022-02-14 15:43:30 +0900

    fix dist -> lib

--- example/greeting.js
+++ example/greeting.js
@@ -1,9 +1,9 @@
-const FastBus = require('../dist');
+const FastBus = require('../lib');
 
 const bus = FastBus.create({ prefix: 'bus', redis: { host: 'localhost', port: 6379, db: 0 } });
 
-bus.subscribe('greeting', message => console.log('hello', message));
-bus.subscribe('greeting', message => console.log('hi', message));
+bus.subscribe('greeting', (message) => console.log('hello', message));
+bus.subscribe('greeting', (message) => console.log('hi', message));
 
 bus.publish('greeting', 'there');
 // hello, there
