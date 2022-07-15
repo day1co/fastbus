e55d1e25f1418de92dcc5e04494bf21c00764c16
kooks7 <minjaekim@fastcampus.co.kr>
2022-07-15 19:01:36 +0900

    fix: add cloud-pub-sub comment

--- src/cloud-pubsub-bus.ts
+++ src/cloud-pubsub-bus.ts
@@ -73,24 +73,33 @@ export class CloudPubSubBus implements BaseBus {
   }
 
   private onSubscription(subscriptionName: string, listener: FastBusSubscriber) {
+    // 1. this.subscribe() 메소드를 호출할때마다 evenEmitter 생성
     this.eventEmitter.on(subscriptionName, listener);
 
+    // 2. GCP pub/sub의 subscriptionName으로 생성된 인스턴스를 가져옴 (싱글톤)
     let subscribeClient = this.getSubscribeClient(subscriptionName);
+    // 3. 생성된 인스턴스가 없으면 새로운 인스턴스를 생성
     if (!subscribeClient) {
+      // pubsub.subscription을 사용해 subscriptionName 기준으로 인스턴스 생성 후 this.subscriptions 객체에 저장
       this.subscriptions[subscriptionName] = this.pubsub.subscription(subscriptionName);
       subscribeClient = this.getSubscribeClient(subscriptionName);
     }
 
+    // 4. subscribeClient에 리스너가 없으면
     if (subscribeClient.listenerCount('message') === 0) {
+      // 5. subscribeClient에 리스너가 없으면 pub/sub client 리스너를 등록
       subscribeClient
         .on('message', (message: Message) => {
+          // 6. node event emitter 가 없으면 메시지를 수신할 수 없다는 의미로, nack 신호를 보냄 (다른 노드 프로세스의 컨슈머가 처리하기를 의도함)
           if (this.eventEmitter.listenerCount(subscriptionName) === 0) {
             this.getSubscribeClient(subscriptionName).removeAllListeners();
             message.nack();
             return;
           }
           logger.debug(`received message: id ${message.id}, data ${message.data}`);
+          // 7. 94 line을 통과한 것은 node 프로세스에 처리해줄 listener가 등록 되어있다는 의미로 message ack 신호를 보냄
           message.ack();
+          // 8. node 프로세스에 등록된 evenEmitter의 listener를 가져와 실행함
           const listener = this.eventEmitter.listeners(subscriptionName)[0];
           listener(message.data.toString());
         })
@@ -99,6 +108,9 @@ export class CloudPubSubBus implements BaseBus {
         });
     }
 
+    // 9. this.subscriptions에 subscriptionName 인스턴스가 존재하고 listener가 존재하지만 closed 상황일때 (subscribeClient 인스턴스의 isOpen 값이 false 일 때),
+    //    this.subscribe() 메소드가 한번 더 호출 되면 새로운 pub/sub lister를 등록하지 않고 (89~109 line), isOpen을 true 상태로 변경함.
+    //    ** this.subscription에 저장된 pubsub client 인스턴스에 listener 갯수가 0인 상황에서는 자동으로 isOpen = false 상태가 되고, 더 이상 메시지를 받을 수 없는 상태가 됨. **
     if (!subscribeClient.isOpen) {
       subscribeClient.open();
     }
